6c6584710053db74d1a68bc7247e3e447bac19f0
Dzavid <dbrdar1@etf.unsa.ba>
2018-12-25 04:07:23 +0100

    Main uradjen

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -6,18 +6,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Gradovi su:\n" + ispisiGradove());
+        System.out.println(ispisiGradove());
         glavniGrad();
     }
 
     private static void glavniGrad() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
         Scanner ulaz = new Scanner(System.in);
-        System.out.println("Unesite naziv drzave:");
-        String nazivDrzave = ulaz.next();
-
+        System.out.println("Unesite naziv države:");
+        String nazivDrzave = ulaz.nextLine();
+        if (dao.nadjiDrzavu(nazivDrzave) == null) System.out.println("Nepostojeća država");
+        else {
+            Drzava drzava = dao.nadjiDrzavu(nazivDrzave);
+            System.out.println("Glavni grad države " + drzava.getNaziv() + " je " + drzava.getGlavniGrad().getNaziv());
+        }
     }
 
     public static String ispisiGradove() {
-        return "";
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        String sadrzaj = "";
+        for (Grad grad : dao.gradovi()) {
+            sadrzaj += grad.getNaziv();
+            sadrzaj += " ";
+            sadrzaj += "(";
+            sadrzaj += grad.getDrzava().getNaziv();
+            sadrzaj += ") - ";
+            sadrzaj += grad.getBrojStanovnika();
+            sadrzaj += "\n";
+        }
+        return sadrzaj;
     }
 }
