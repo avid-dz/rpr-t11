c926213219e5af0b4bf9df76cf15d2df170a6c05
Dzavid <dbrdar1@etf.unsa.ba>
2019-01-03 12:16:25 +0100

    Totalni refactor

---
+++ src/main/java/ba/unsa/etf/rpr/GradoviReport.java
@@ -0,0 +1,96 @@
+package ba.unsa.etf.rpr;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleDocxReportConfiguration;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+import net.sf.jasperreports.swing.JRViewer;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GradoviReport extends JFrame{
+
+    public void saveAs(Connection conn, String format) throws JRException, FileNotFoundException {
+        File file = new File(format);
+        String reportSrcFile = getClass().getResource("/main/resources/reports/gradovi.jrxml").getFile();
+        String reportsDir = getClass().getResource("/main/resources/reports/").getFile();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
+        // Fields for resources path
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("reportsDirPath", reportsDir);
+        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        list.add(parameters);
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, conn);
+
+        if (format.contains(".pdf")) {
+            JasperExportManager.exportReportToPdfStream(print, new FileOutputStream(file));
+        }
+        else if (format.contains(".docx")) {
+            JRDocxExporter export = new JRDocxExporter();
+            export.setExporterInput(new SimpleExporterInput(print));
+            export.setExporterOutput(new SimpleOutputStreamExporterOutput(file));
+            SimpleDocxReportConfiguration config = new SimpleDocxReportConfiguration();
+            export.setConfiguration(config);
+            export.exportReport();
+        }
+        else if (format.contains(".xlsx")) {
+            JRXlsxExporter export = new JRXlsxExporter();
+            export.setExporterInput(new SimpleExporterInput(print));
+            export.setExporterOutput(new SimpleOutputStreamExporterOutput(file));
+            SimpleXlsxReportConfiguration config = new SimpleXlsxReportConfiguration();
+            config.setOnePagePerSheet(true);
+            export.setConfiguration(config);
+            export.exportReport();
+        }
+    }
+
+    public void showReport(Connection conn) throws JRException {
+        String reportSrcFile = getClass().getResource("/main/resources/reports/gradovi.jrxml").getFile();
+        String reportsDir = getClass().getResource("/main/resources/reports/").getFile();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
+        // Fields for resources path
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("reportsDirPath", reportsDir);
+        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        list.add(parameters);
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, conn);
+        JRViewer viewer = new JRViewer(print);
+        viewer.setOpaque(true);
+        viewer.setVisible(true);
+        this.add(viewer);
+        this.setSize(700, 500);
+        this.setVisible(true);
+    }
+
+    public void showReport1(Connection conn, String nazivDrzave) throws JRException {
+        String reportSrcFile = getClass().getResource("/main/resources/reports/gradovi1.jrxml").getFile();
+        String reportsDir = getClass().getResource("/main/resources/reports/").getFile();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
+        // Fields for resources path
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("reportsDirPath", reportsDir);
+        String pa = nazivDrzave;
+        parameters.put("pa", pa);
+        ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+        list.add(parameters);
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, conn);
+        JRViewer viewer = new JRViewer(print);
+        viewer.setOpaque(true);
+        viewer.setVisible(true);
+        this.add(viewer);
+        this.setSize(700, 500);
+        this.setVisible(true);
+    }
+}
---
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,69 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO geo=GeografijaDAO.getInstance();
+        Locale.setDefault(new Locale("bs", "BA"));
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        Parent root = FXMLLoader.load(getClass().getResource("src/main/java/ba/unsa/etf/rpr/izbor.fxml"), bundle);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("GUI");
+        primaryStage.setScene(new Scene(root, 300, 200));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        /*System.out.println(ispisiGradove());
+        glavniGrad();*/
+        launch(args);
+    }
+
+    private static void glavniGrad() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite naziv države:");
+        String nazivDrzave = ulaz.nextLine();
+        if (dao.nadjiDrzavu(nazivDrzave) == null) System.out.println("Nepostojeća država");
+        else {
+            Drzava drzava = dao.nadjiDrzavu(nazivDrzave);
+            System.out.println("Glavni grad države " + drzava.getNaziv() + " je " + drzava.getGlavniGrad().getNaziv());
+        }
+    }
+
+    public static String ispisiGradove() {
+        GeografijaDAO.removeInstance();
+        File dbfile = new File("baza.db");
+        dbfile.delete();
+        GeografijaDAO dao = GeografijaDAO.getInstance();
+        String sadrzaj = "";
+        for (Grad grad : dao.gradovi()) {
+            sadrzaj += grad.getNaziv();
+            sadrzaj += " ";
+            sadrzaj += "(";
+            sadrzaj += grad.getDrzava().getNaziv();
+            sadrzaj += ") - ";
+            sadrzaj += grad.getBrojStanovnika();
+            sadrzaj += "\n";
+        }
+        return sadrzaj;
+    }
+}
